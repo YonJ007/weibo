3aff9c8de27c7f41478590e4107186caeafacd19
2802309050@qq.com <2802309050@qq.com>
2022-07-12 10:35:31 +0800

    静态页面和模板切分

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','about us')
+@section('content')
+    <h1>关于页</h1>
+@endsection
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','weibo app')
+@section('content')
+    <h1>主页</h1>
+@endsection
